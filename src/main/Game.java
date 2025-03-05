fb7d4faf9c478553475edd59f0d915b558f76daf
Aaron Ragudos <workemailaaronragudos@gmail.com>
2025-03-06 01:04:32 +0800

    refactor Game and GameScreen, and add game loop and multi-threading

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,41 +1,131 @@
 package main;
 
-import javax.imageio.ImageIO;
+import inputs.KeyboardListener;
+import inputs.TowerDefenseMouseListener;
+import scenes.MenuScene;
+import scenes.PlayingScene;
+import scenes.SettingsScene;
+
 import javax.swing.*;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.io.InputStream;
 
-public class Game extends JFrame {
+public class Game extends JFrame implements Runnable {
+    // Settings
+    private double FPS_SET = 120.0;
+    private double UPS_SET = 60.0;
 
+    // Listeners
+    private TowerDefenseMouseListener mouseListener;
+    private KeyboardListener keyboardListener;
+
+    // Classes
+    private Thread gameThread;
     private GameScreen gameScreen;
-    private BufferedImage img;
+    private Renderer renderer;
+    private MenuScene menuScene;
+    private SettingsScene settingsScene;
+    private PlayingScene playingScene;
 
     public Game() {
-        this.importImg();
-
-        this.setSize(640, 640);
-        this.setVisible(true);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        this.initClasses();
+        this.add(this.gameScreen);
+
+        this.pack();
         this.setLocationRelativeTo(null);
-
-        gameScreen = new GameScreen(img);
-
-        this.add(gameScreen);
+        this.setVisible(true);
     }
 
-    private void importImg() {
-        InputStream is = getClass().getResourceAsStream("/spriteatlas.png");
+    public Renderer getRenderer() {
+        return this.renderer;
+    }
 
-        try {
-            assert is != null;
-            img = ImageIO.read(is);
-        } catch (IOException e) {
-            e.printStackTrace();
+    public MenuScene getMenuScene() {
+        return menuScene;
+    }
+
+    public SettingsScene getSettingsScene() {
+        return settingsScene;
+    }
+
+    public PlayingScene getPlayingScene() {
+        return playingScene;
+    }
+
+    @Override
+    public void run() {
+        double timePerFrame = 1000000000.0 / FPS_SET;
+        double timePerUpdate = 1000000000.0 / UPS_SET;
+
+        long lastFrame = System.nanoTime();
+        long lastUpdate = System.nanoTime();
+        long lastTimeCheckInMs = System.currentTimeMillis();
+
+        int frames = 0;
+        int updates = 0;
+
+        long nowInNanoTime;
+        long nowInMilliTime;
+
+        while (true) {
+            nowInNanoTime = System.nanoTime();
+            nowInMilliTime = System.currentTimeMillis();
+
+            // Render
+            if (nowInNanoTime - lastFrame >= timePerFrame) {
+                gameScreen.repaint();
+
+                lastFrame = nowInNanoTime;
+                frames += 1;
+            }
+
+            // Update
+            if (nowInNanoTime - lastUpdate >= timePerUpdate) {
+                this.updateGame();
+
+                lastUpdate = nowInNanoTime;
+                updates += 1;
+            }
+
+            if (nowInMilliTime - lastTimeCheckInMs >= 1000) {
+                System.out.println("FPS: " + frames +" | UPS: " + updates);
+
+                frames = 0;
+                updates = 0;
+                lastTimeCheckInMs = nowInMilliTime;
+            }
         }
     }
 
+    private void initClasses() {
+        this.gameThread = new Thread(this) {};
+        this.renderer = new Renderer(this);
+        this.gameScreen = new GameScreen(this);
+        this.menuScene = new MenuScene(this);
+        this.settingsScene = new SettingsScene(this);
+        this.playingScene = new PlayingScene(this);
+    }
+
+    private void initInputs() {
+        this.mouseListener = new TowerDefenseMouseListener();
+        this.keyboardListener = new KeyboardListener();
+
+        this.addMouseListener(this.mouseListener);
+        this.addMouseMotionListener(this.mouseListener);
+        this.addKeyListener(this.keyboardListener);
+
+        this.requestFocus();
+    }
+
+    private void start () {
+        gameThread.start();
+    }
+
+    private void updateGame() {}
+
     public static void main(String[] args) {
         Game game = new Game();
+        game.initInputs();
+        game.start();
     }
 }
--- src/main/GameScreen.java
+++ src/main/GameScreen.java
@@ -2,55 +2,27 @@ package main;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.util.ArrayList;
-import java.util.Random;
 
 public class GameScreen extends JPanel {
-    private Random random;
+    private Game game;
+    private Dimension size;
 
-    private BufferedImage img;
-    private ArrayList<BufferedImage> sprites;
+    public GameScreen(Game game) {
+        this.game = game;
 
-    public GameScreen(BufferedImage img) {
-        this.sprites = new ArrayList<>();
-        this.img = img;
-        this.random = new Random();
-
-        this.loadSprites();
-    }
-
-    private void loadSprites() {
-        for (int y = 0; y < 3; ++y) {
-            for (int x = 0; x < 10; ++x) {
-                this.sprites.add(this.img.getSubimage(x * 32, y * 32, 32, 32));
-            }
-        }
+        this.setPanelSize();
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-
-        for (int y = 0; y < 20; ++y) {
-            for (int x = 0; x < 20; ++x) {
-                g.drawImage(this.sprites.get(this.getRandomInt()), x * 32, y * 32, null);
-            }
-        }
-
+        this.game.getRenderer().render(g);
     }
 
-    private int getRandomInt() {
-        return this.random.nextInt(this.sprites.size());
+    private void setPanelSize() {
+        this.size = new Dimension(640, 640);
+
+        this.setMinimumSize(this.size);
+        this.setMaximumSize(this.size);
+        this.setPreferredSize(this.size);
     }
-
-    private Color getRandomColor() {
-        int r, g, b;
-
-        r = this.random.nextInt(256);
-        g = this.random.nextInt(256);
-        b = this.random.nextInt(256);
-
-        return new Color(r, g, b);
-    }
-
 }
