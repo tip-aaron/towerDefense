d567349aa5e843552d99b23caa9a3f0261f39bc2
Aaron Ragudos <workemailaaronragudos@gmail.com>
2025-03-08 23:35:27 +0800

    update TowerDefenseMouseListener to accommodate mouse clicks and show responsiveness in MenuScene

--- src/inputs/TowerDefenseMouseListener.java
+++ src/inputs/TowerDefenseMouseListener.java
@@ -1,23 +1,60 @@
 package inputs;
 
+import main.Game;
+import main.GameStates;
+
+import javax.swing.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 
-public class TowerDefenseMouseListener implements MouseListener, MouseMotionListener {
+public class TowerDefenseMouseListener implements MouseListener,
+        MouseMotionListener {
+    private Game game;
+
+    public TowerDefenseMouseListener(Game game) {
+        this.game = game;
+    }
+
     @Override
     public void mouseClicked(MouseEvent e) {
-
+        switch (GameStates.gameState) {
+            case MENU:
+                game.getMenu().mouseClicked(e.getX(), e.getY());
+                break;
+            case PLAYING:
+                break;
+            case SETTINGS:
+                break;
+        }
     }
 
     @Override
     public void mousePressed(MouseEvent e) {
-
+        if (e.getButton() == MouseEvent.BUTTON1) {
+            switch (GameStates.gameState) {
+                case MENU:
+                    this.game.getMenu().mousePressed(e.getX(), e.getY());
+                    break;
+                case PLAYING:
+                    break;
+                case SETTINGS:
+                    break;
+            }
+        }
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
-
+        switch (GameStates.gameState) {
+            case MENU:
+                game.getMenu().mouseReleased(e.getX(), e.getY());
+                break;
+            case PLAYING:
+                break;
+            case SETTINGS:
+                break;
+        }
     }
 
     @Override
@@ -37,6 +74,14 @@ public class TowerDefenseMouseListener implements MouseListener, MouseMotionList
 
     @Override
     public void mouseMoved(MouseEvent e) {
-
+        switch (GameStates.gameState) {
+            case MENU:
+                this.game.getMenu().mouseMoved(e.getX(), e.getY());
+                break;
+            case PLAYING:
+                break;
+            case SETTINGS:
+                break;
+        }
     }
 }
